995376e5de5bf2e4b9a1c9d44cebdac3260d9edc
HaRo87 <robert@fam-hansel.de>
2018-06-06 12:14:02 +0200

    #1: added new lines at end of files

--- remocar-control/io/test_config.py
+++ remocar-control/io/test_config.py
@@ -12,3 +12,4 @@ def test_motorconfig_high_pin_violation_lower_boundary_thows_error():
     with pytest.raises(ControlConfigError) as err_info:
         motorconfig = MotorConfig(MotorType.MOVEMENT, 0, 3, 23)
     assert str(err_info.value) == "(0, 'must be >=2 and <= 26!')"
+    
